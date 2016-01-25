0f3e167ac49c1274c6c5d8438f96fdeffe553067
Kev <spam4kev@gmail.com>
2016-01-25 14:20:48 -0800

    fixed incorrect sed syntax

--- install-packstack.sh
+++ install-packstack.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#!/bin/sh
 # check if argument was passed. If not exit.
 [[ -z $1 ]] && echo "please pass the first argument containing the desired openstack password" && exit
 [[ -z $2 ]] && echo "please pass the second argument containing the openstack hosts physical NIC IP" && exit
@@ -12,6 +11,6 @@ sudo mv /etc/sysconfig/networking-scripts/ifcfg-enp2s0 ~/ifcfg-enp2s0-$(date +%s
 sudo mv /etc/sysconfig/networking-scripts/ifcfg-br-ex ~/ifcfg-br-ex-$(date +%s)
 sudo cp ./ifcfg-enp2s0 /etc/sysconfig/networking-scripts/
 sudo cp ./ifcfg-br-ex /etc/sysconfig/networking-scripts/
-sed -i "r/ip/$openstack_host_priv_ip/g" /etc/sysconfig/networking-scripts/ifcfg-br-ex
-sed -i "r/gateway/$gw/g" /etc/sysconfig/networking-scripts/ifcfg-br-ex
-sed -i "r/macaddress/$mac/g" /etc/sysconfig/networking-scripts/ifcfg-enp2s0
+sed -i "s/ip/$openstack_host_priv_ip/g" /etc/sysconfig/networking-scripts/ifcfg-br-ex
+sed -i "s/gateway/$gw/g" /etc/sysconfig/networking-scripts/ifcfg-br-ex
+sed -i "s/macaddress/$mac/g" /etc/sysconfig/networking-scripts/ifcfg-enp2s0
