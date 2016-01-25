e286049cca31b326eca7e1e1c53197f25526ae53
Kev <spam4kev@gmail.com>
2016-01-25 15:08:50 -0800

    wip

--- install-packstack.sh
+++ install-packstack.sh
@@ -17,3 +17,13 @@ sudo cp ./ifcfg-br-ex /etc/sysconfig/networking-scripts/
 sed -i "s/ip/$openstack_host_priv_ip/g" /etc/sysconfig/networking-scripts/ifcfg-br-ex
 sed -i "s/gateway/$gw/g" /etc/sysconfig/networking-scripts/ifcfg-br-ex
 sed -i "s/macaddress/$mac/g" /etc/sysconfig/networking-scripts/ifcfg-enp2s0
+sudo openstack-config --set /etc/neutron/plugins/ml2/openvswitch_agent.ini ovs bridge_mappings extnet:br-ex
+sudo openstack-config --set /etc/neutron/plugin.ini ml2 type_drivers vxlan,flat,vlan
+sudo sh -c "systemctl restart network.service && systemctl restart neutron-openvswitch-agent.service && systemctl restart neutron-server.service"
+sudo openstack-config --set /etc/neutron/plugin.ini ml2_type_flat flat_networks physnet1,extnet
+cd ~
+source keystonerc_admin
+neutron net-create external_network --provider:network_type flat --provider:physical_network extnet  --router:external --shared
+sudo openstack-config --set /etc/nova/nova.conf DEFAULT compute_driver libvirt.LibvirtDriver
+sudo openstack-config --set /etc/nova/nova.conf DEFAULT dhcp_domain fitznet.local
+sudo systemctl restart openstack-nova-compute.service
